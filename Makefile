a81ec6fc6640d872ad84d2ce0faca85aee9c36de
Mark Hutcheson <meh2481@vt.edu>
2013-09-13 18:43:01 -0400

    Add more commandline options for decompression

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
 SHELL=C:/Windows/System32/cmd.exe
 objects = main.o wfLZ.o
 LIBPATH = -L./lib
-LIB = -lsquish -lpng -lzlib -lFreeImage
+LIB = -lsquish -lFreeImage
 HEADERPATH = -I./include
 STATICGCC = -static-libgcc -static-libstdc++
 
 all : wfLZEx.exe
  
 wfLZEx.exe : $(objects)
-	g++ -Wall -O2 -o $@ $(objects) $(LIBPATH) $(LIB) $(STATICGCC) $(HEADERPATH)
+	g++ -Wall -O2 -s -o $@ $(objects) $(LIBPATH) $(LIB) $(STATICGCC) $(HEADERPATH)
 	
 %.o: %.cpp
-	g++ -O2 -c -MMD -o $@ $< $(HEADERPATH)
+	g++ -O2 -c -MMD -s -o $@ $< $(HEADERPATH)
 
 -include $(objects:.o=.d)
 
--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@
 #include <cmath>
 using namespace std;
 
+int g_DecompressFlags;
+bool g_bSeparate;
+bool g_bPieceTogether;
+bool g_bColOnly;
+bool g_bMulOnly;
+
 typedef struct
 {
 	uint32_t unknown0;
@@ -49,6 +55,154 @@ typedef struct
 	//piece[]
 } PiecesDesc;
 
+int powerof2(int orig)
+{
+	int result = 1;
+	while(result < orig)
+		result <<= 1;
+	return result;
+}
+
+void multiply(uint8_t* dest_final, uint8_t* color, uint8_t* mul, texHeader th)
+{
+	//Multiply two images together
+	for(int j = 0; j < th.width * th.height * 4; j += 4)
+	{
+		if(color[j+3] != 255)
+		{
+			dest_final[j] = dest_final[j+1] = dest_final[j+2] = 0;
+			dest_final[j+3] = mul[j+1];
+		}
+		else
+		{
+			uint8_t mask = 255 - mul[j+1];
+			float fMul = (float)(mask)/255.0;
+			dest_final[j] = color[j]*fMul;
+			dest_final[j+1] = color[j+1]*fMul;
+			dest_final[j+2] = color[j+2]*fMul;
+			dest_final[j+3] = 255;
+		}				
+	}
+}
+
+FIBITMAP* imageFromPixels(uint8_t* imgData, uint32_t width, uint32_t height)
+{
+	//return FreeImage_ConvertFromRawBits(imgData, width, height, width*4, 32, 0xFF0000, 0x00FF00, 0x0000FF, true);	//Doesn't seem to work
+	FIBITMAP* curImg = FreeImage_Allocate(width, height, 32);
+	FREE_IMAGE_TYPE image_type = FreeImage_GetImageType(curImg);
+	if(image_type == FIT_BITMAP)
+	{
+		int curPos = 0;
+		unsigned pitch = FreeImage_GetPitch(curImg);
+		BYTE* bits = (BYTE*)FreeImage_GetBits(curImg);
+		bits += pitch * height - pitch;
+		for(int y = height-1; y >= 0; y--)
+		{
+			BYTE* pixel = (BYTE*)bits;
+			for(int x = 0; x < width; x++)
+			{
+				pixel[FI_RGBA_RED] = imgData[curPos++];
+				pixel[FI_RGBA_GREEN] = imgData[curPos++];
+				pixel[FI_RGBA_BLUE] = imgData[curPos++];
+				pixel[FI_RGBA_ALPHA] = imgData[curPos++];
+				pixel += 4;
+			}
+			bits -= pitch;
+		}
+	}
+	return curImg;
+}
+
+FIBITMAP* PieceImage(uint8_t* imgData, list<piece> pieces, Vec2 maxul, Vec2 maxbr, texHeader th, bool bFillBlack = false, bool bAdd = true);
+
+FIBITMAP* PieceImage(uint8_t* imgData, list<piece> pieces, Vec2 maxul, Vec2 maxbr, texHeader th, bool bFillBlack, bool bAdd)
+{
+	Vec2 OutputSize;
+	Vec2 CenterPos;
+	OutputSize.x = -maxul.x + maxbr.x;
+	OutputSize.y = maxul.y - maxbr.y;
+	CenterPos.x = -maxul.x;
+	CenterPos.y = maxul.y;
+	OutputSize.x = uint32_t(OutputSize.x);
+	OutputSize.y = uint32_t(OutputSize.y);
+
+	FIBITMAP* result = FreeImage_Allocate(OutputSize.x+6, OutputSize.y+6, 32);
+	
+	//Fill this image black (Important for multiply images)
+	if(bFillBlack)
+	{
+		int width = FreeImage_GetWidth(result);
+		int height = FreeImage_GetHeight(result);
+		unsigned pitch = FreeImage_GetPitch(result);
+		BYTE* bits = (BYTE*)FreeImage_GetBits(result);
+		for(int y = 0; y < height; y++)
+		{
+			BYTE* pixel = bits;
+			for(int x = 0; x < width; x++)
+			{
+				pixel[FI_RGBA_RED] = pixel[FI_RGBA_GREEN] = pixel[FI_RGBA_BLUE] = 0;
+				pixel[FI_RGBA_ALPHA] = 255;
+				pixel += 4;
+			}
+			bits += pitch;
+		}
+	}
+
+	//Create image from this set of pixels
+	FIBITMAP* curImg = imageFromPixels(imgData, th.width, th.height);
+
+	//Patch image together from pieces
+	for(list<piece>::iterator lpi = pieces.begin(); lpi != pieces.end(); lpi++)
+	{
+		float add = 0;
+		if(bAdd)
+			add = 0.001;
+		FIBITMAP* imgPiece = FreeImage_Copy(curImg, 
+											floor((lpi->topLeftUV.x) * th.width - add), floor((lpi->topLeftUV.y) * th.height - add), 
+											ceil((lpi->bottomRightUV.x) * th.width + add), ceil((lpi->bottomRightUV.y) * th.height + add));
+		
+		//Since pasting doesn't allow you to post an image onto a particular position of another, do that by hand
+		int curPos = 0;
+		int srcW = FreeImage_GetWidth(imgPiece);
+		int srcH = FreeImage_GetHeight(imgPiece);
+		unsigned pitch = FreeImage_GetPitch(imgPiece);
+		unsigned destpitch = FreeImage_GetPitch(result);
+		BYTE* bits = (BYTE*)FreeImage_GetBits(imgPiece);
+		BYTE* destBits = (BYTE*)FreeImage_GetBits(result);
+		Vec2 DestPos = CenterPos;
+		DestPos.x += lpi->topLeft.x;
+		//DestPos.y -= lpi->topLeft.y;
+		DestPos.y = OutputSize.y - srcH;
+		DestPos.y -= CenterPos.y;
+		DestPos.y += lpi->topLeft.y;
+		DestPos.x = (unsigned int)(DestPos.x);
+		DestPos.y = ceil(DestPos.y);
+		for(int y = 0; y < srcH; y++)
+		{
+			BYTE* pixel = bits;
+			BYTE* destpixel = destBits;
+			destpixel += (unsigned)((DestPos.y + y + 3)) * destpitch;
+			destpixel += (unsigned)((DestPos.x + 3) * 4);
+			for(int x = 0; x < srcW; x++)
+			{
+				destpixel[FI_RGBA_RED] = pixel[FI_RGBA_RED];
+				destpixel[FI_RGBA_GREEN] = pixel[FI_RGBA_GREEN];
+				destpixel[FI_RGBA_BLUE] = pixel[FI_RGBA_BLUE];
+				//if(pixel[FI_RGBA_ALPHA] != 0)
+					destpixel[FI_RGBA_ALPHA] = pixel[FI_RGBA_ALPHA];
+				pixel += 4;
+				destpixel += 4;
+			}
+			bits += pitch;
+		}
+		
+		FreeImage_Unload(imgPiece);
+	}
+	FreeImage_Unload(curImg);
+	
+	return result;
+}
+
 
 int splitImages(const char* cFilename)
 {
@@ -73,23 +227,31 @@ int splitImages(const char* cFilename)
 	{
 		if(memcmp ( &(vData.data()[i]), "ZLFW", 4 ) == 0)	//Found another file
 		{
+			//Get texture header
 			uint64_t headerPos = i - sizeof(texHeader);
 			texHeader th;
 			memcpy ( &th, &(vData.data()[headerPos]), sizeof(texHeader) );
-			if(iCurFile == 0)	//First one
-				startPos = i;
-			FrameDesc fd;
-			for(int k = startPos - sizeof(texHeader) - sizeof(FrameDesc); k > 0; k -= 4)
-			{
-				memcpy(&fd, &(vData.data()[k]), sizeof(FrameDesc));
-				if(fd.texOffset != headerPos) continue;
-				//Sanity check
-				if(fd.texDataSize > 6475888) continue;
-				if(fd.texOffset == 0 || fd.pieceOffset == 0) continue;
 				
-				//Ok, found our header
-				break;
+			//Search for frame header if we're going to be piecing these together
+			FrameDesc fd;
+			if(g_bPieceTogether)
+			{
+				if(iCurFile == 0)	//First one tells us where to start searching backwards from
+					startPos = i;
+				for(int k = startPos - sizeof(texHeader) - sizeof(FrameDesc); k > 0; k -= 4)
+				{
+					memcpy(&fd, &(vData.data()[k]), sizeof(FrameDesc));
+					if(fd.texOffset != headerPos) continue;
+					//Sanity check
+					if(fd.texDataSize > 6475888) continue;
+					if(fd.texOffset == 0 || fd.pieceOffset == 0) continue;
+					
+					//Ok, found our header
+					break;
+				}
 			}
+			
+			//Decompress WFLZ data
 			uint32_t* chunk = NULL;
 			const uint32_t decompressedSize = wfLZ_GetDecompressedSize( &(vData.data()[i]) );
 			uint8_t* dst = ( uint8_t* )malloc( decompressedSize );
@@ -102,150 +264,123 @@ int splitImages(const char* cFilename)
 				offset += blockSize;
 			}
 			
+			//Create color image
 			uint8_t* color = (uint8_t*)malloc(decompressedSize * 8);
+			if(!g_bMulOnly)
+				squish::DecompressImage( color, th.width, th.height, dst, g_DecompressFlags );
 			
-			squish::DecompressImage( color, th.width, th.height, dst, squish::kDxt1 );
-			
-			//Create second image
+			//Create multiply image
 			uint8_t* mul = (uint8_t*)malloc(decompressedSize * 8);
-			squish::DecompressImage( mul, th.width, th.height, dst + decompressedSize/2, squish::kDxt1 );	//Second image starts halfway through decompressed data
-			
-			
-			uint8_t* dest_final = (uint8_t*)malloc(decompressedSize * 8);
-			//Multiply together
-			for(int j = 0; j < th.width * th.height * 4; j+=4)
-			{
-				if(color[j+3] != 255)
-				{
-					dest_final[j] = dest_final[j+1] = dest_final[j+2] = 0;
-					dest_final[j+3] = mul[j+1];
-				}
-				else
-				{
-					uint8_t mask = 255 - mul[j+1];
-					float fMul = (float)(mask)/255.0;
-					dest_final[j] = color[j]*fMul;
-					dest_final[j+1] = color[j+1]*fMul;
-					dest_final[j+2] = color[j+2]*fMul;
-					dest_final[j+3] = 255;
-				}				
-			}
+			if(!g_bColOnly)
+				squish::DecompressImage( mul, th.width, th.height, dst + decompressedSize/2, g_DecompressFlags );	//Second image starts halfway through decompressed data
 			
 			//Read in pieces
-			PiecesDesc pd;
 			Vec2 maxul;
 			Vec2 maxbr;
 			maxul.x = maxul.y = maxbr.x = maxbr.y = 0;
-			memcpy(&pd, &(vData.data()[fd.pieceOffset]), sizeof(PiecesDesc));
 			list<piece> pieces;
-			for(uint32_t j = 0; j < pd.numPieces; j++)
+			
+			if(g_bPieceTogether)
 			{
-				piece p;
-				memcpy(&p, &(vData.data()[fd.pieceOffset+j*sizeof(piece)+sizeof(PiecesDesc)]), sizeof(piece));
-				//Store our maximum values, so we know how large the image is
-				if(p.topLeft.x < maxul.x)
-					maxul.x = p.topLeft.x;
-				if(p.topLeft.y > maxul.y)
-					maxul.y = p.topLeft.y;
-				if(p.bottomRight.x > maxbr.x)
-					maxbr.x = p.bottomRight.x;
-				if(p.bottomRight.y < maxbr.y)
-					maxbr.y = p.bottomRight.y;
-				pieces.push_back(p);
-			}
-			
-			Vec2 OutputSize;
-			Vec2 CenterPos;
-			OutputSize.x = -maxul.x + maxbr.x;
-			OutputSize.y = maxul.y - maxbr.y;
-			CenterPos.x = -maxul.x;
-			CenterPos.y = maxul.y;
-			OutputSize.x = uint32_t(OutputSize.x);
-			OutputSize.y = uint32_t(OutputSize.y);
-			
-			FIBITMAP* result = FreeImage_Allocate(OutputSize.x+6, OutputSize.y+6, 32);
-			
-			//Create image from this set of pixels
-			FIBITMAP* curImg = FreeImage_Allocate(th.width, th.height, 32);
-			FREE_IMAGE_TYPE image_type = FreeImage_GetImageType(curImg);
-			if(image_type == FIT_BITMAP)
-			{
-				int curPos = 0;
-				unsigned pitch = FreeImage_GetPitch(curImg);
-				BYTE* bits = (BYTE*)FreeImage_GetBits(curImg);
-				bits += pitch * th.height - pitch;
-				for(int y = th.height-1; y >= 0; y--)
+				PiecesDesc pd;
+				memcpy(&pd, &(vData.data()[fd.pieceOffset]), sizeof(PiecesDesc));
+				for(uint32_t j = 0; j < pd.numPieces; j++)
 				{
-					BYTE* pixel = (BYTE*)bits;
-					for(int x = 0; x < th.width; x++)
-					{
-						pixel[FI_RGBA_RED] = dest_final[curPos++];
-						pixel[FI_RGBA_GREEN] = dest_final[curPos++];
-						pixel[FI_RGBA_BLUE] = dest_final[curPos++];
-						pixel[FI_RGBA_ALPHA] = dest_final[curPos++];
-						pixel += 4;
-					}
-					bits -= pitch;
+					piece p;
+					memcpy(&p, &(vData.data()[fd.pieceOffset+j*sizeof(piece)+sizeof(PiecesDesc)]), sizeof(piece));
+					//Store our maximum values, so we know how large the image is
+					if(p.topLeft.x < maxul.x)
+						maxul.x = p.topLeft.x;
+					if(p.topLeft.y > maxul.y)
+						maxul.y = p.topLeft.y;
+					if(p.bottomRight.x > maxbr.x)
+						maxbr.x = p.bottomRight.x;
+					if(p.bottomRight.y < maxbr.y)
+						maxbr.y = p.bottomRight.y;
+					pieces.push_back(p);
 				}
 			}
 			
-			//Patch image together from pieces
-			for(list<piece>::iterator lpi = pieces.begin(); lpi != pieces.end(); lpi++)
+			//Merge images together and piece them up
+			if(!g_bSeparate && g_bPieceTogether)
 			{
-				FIBITMAP* imgPiece = FreeImage_Copy(curImg, 
-													floor((lpi->topLeftUV.x) * th.width - 0.001), floor((lpi->topLeftUV.y) * th.height - 0.001), 
-													ceil((lpi->bottomRightUV.x) * th.width + 0.001), ceil((lpi->bottomRightUV.y) * th.height + 0.001));
+				uint8_t* dest_final = (uint8_t*)malloc(decompressedSize * 8);
+				multiply(dest_final, color, mul, th);
+			
+				FIBITMAP* result = PieceImage(dest_final, pieces, maxul, maxbr, th);
 				
-				//Since pasting doesn't allow you to post an image onto a particular position of another, do that by hand
-				int curPos = 0;
-				int srcW = FreeImage_GetWidth(imgPiece);
-				int srcH = FreeImage_GetHeight(imgPiece);
-				unsigned pitch = FreeImage_GetPitch(imgPiece);
-				unsigned destpitch = FreeImage_GetPitch(result);
-				BYTE* bits = (BYTE*)FreeImage_GetBits(imgPiece);
-				BYTE* destBits = (BYTE*)FreeImage_GetBits(result);
-				Vec2 DestPos = CenterPos;
-				DestPos.x += lpi->topLeft.x;
-				//DestPos.y -= lpi->topLeft.y;
-				DestPos.y = OutputSize.y - srcH;
-				DestPos.y -= CenterPos.y;
-				DestPos.y += lpi->topLeft.y;
-				DestPos.x = (unsigned int)(DestPos.x);
-				DestPos.y = ceil(DestPos.y);
-				for(int y = 0; y < srcH; y++)
+				char cOutName[256];
+				sprintf(cOutName, "output/%s-%d.png", cFilename, iCurFile);
+				FreeImage_Save(FIF_PNG, result, cOutName);
+				FreeImage_Unload(result);
+				free(dest_final);
+			}
+			//Separate images, pieced together
+			else if(g_bSeparate && g_bPieceTogether)
+			{
+				char cOutName[256];
+				//Save color map
+				if(!g_bMulOnly)
 				{
-					BYTE* pixel = bits;
-					BYTE* destpixel = destBits;
-					destpixel += (unsigned)((DestPos.y + y + 3)) * destpitch;
-					destpixel += (unsigned)((DestPos.x + 3) * 4);
-					for(int x = 0; x < srcW; x++)
-					{
-						destpixel[FI_RGBA_RED] = pixel[FI_RGBA_RED];
-						destpixel[FI_RGBA_GREEN] = pixel[FI_RGBA_GREEN];
-						destpixel[FI_RGBA_BLUE] = pixel[FI_RGBA_BLUE];
-						//if(pixel[FI_RGBA_ALPHA] != 0)
-							destpixel[FI_RGBA_ALPHA] = pixel[FI_RGBA_ALPHA];
-						pixel += 4;
-						destpixel += 4;
-					}
-					bits += pitch;
+					FIBITMAP* result = PieceImage(color, pieces, maxul, maxbr, th, false, false);
+					sprintf(cOutName, "output/%s-%d-col.png", cFilename, iCurFile);
+					FreeImage_Save(FIF_PNG, result, cOutName);
+					FreeImage_Unload(result);
 				}
 				
-				FreeImage_Unload(imgPiece);
-			}			
-			
+				//Save multiply map
+				if(!g_bColOnly)
+				{
+					FIBITMAP* result = PieceImage(mul, pieces, maxul, maxbr, th, true);
+					sprintf(cOutName, "output/%s-%d-mul.png", cFilename, iCurFile);
+					FreeImage_Save(FIF_PNG, result, cOutName);
+					FreeImage_Unload(result);
+				}
+			}
+			//Separate images, not pieced together
+			else if(g_bSeparate && !g_bPieceTogether)
+			{
+				FIBITMAP* result;
+				char cOutName[256];
+				
+				//Save color image
+				if(!g_bMulOnly)
+				{
+					result = imageFromPixels(color, th.width, th.height);
+					sprintf(cOutName, "output/%s-%d-col.png", cFilename, iCurFile);
+					FreeImage_Save(FIF_PNG, result, cOutName);
+					FreeImage_Unload(result);
+				}
+				
+				//Save multiply image
+				if(!g_bColOnly)
+				{
+					result = imageFromPixels(mul, th.width, th.height);
+					sprintf(cOutName, "output/%s-%d-mul.png", cFilename, iCurFile);
+					FreeImage_Save(FIF_PNG, result, cOutName);
+					FreeImage_Unload(result);
+				}
+			}
+			//Same image, not pieced together
+			else if(!g_bSeparate && !g_bPieceTogether)
+			{
+				uint8_t* dest_final = (uint8_t*)malloc(th.width * th.height * 4);
+				multiply(dest_final, color, mul, th);
+				FIBITMAP* result = imageFromPixels(dest_final, th.width, th.height);
+				
+				char cOutName[256];
+				sprintf(cOutName, "output/%s-%d.png", cFilename, iCurFile);
+				FreeImage_Save(FIF_PNG, result, cOutName);
+				FreeImage_Unload(result);
+				
+				free(dest_final);
+			}
+			//Free allocated memory
 			free(dst);
 			free(color);
 			free(mul);
-			free(dest_final);
-			char cOutName[256];
-			sprintf(cOutName, "output/%s-%d.png", cFilename, ++iCurFile);
-			FreeImage_Save(FIF_PNG, result, cOutName);
-			FreeImage_Unload(result);
-			FreeImage_Unload(curImg);
 			
-			//TODO: Remove. For testing purposes, we'll only do one image because faster
-			//break;
+			iCurFile++;
 		}
 	}
 	return 0;
@@ -253,12 +388,45 @@ int splitImages(const char* cFilename)
 
 int main(int argc, char** argv)
 {
+	g_DecompressFlags = squish::kDxt1;
+	g_bSeparate = false;
+	g_bPieceTogether = true;
+	g_bColOnly = g_bMulOnly = false;
 	FreeImage_Initialise();
 	CreateDirectory(TEXT("output"), NULL);
+	list<string> sFilenames;
+	//Parse commandline
 	for(int i = 1; i < argc; i++)
 	{
-		splitImages(argv[i]);
+		string s = argv[i];
+		if(s == "-dxt1")
+			g_DecompressFlags = squish::kDxt1;
+		else if(s == "-dxt3")
+			g_DecompressFlags = squish::kDxt3;
+		else if(s == "-dxt5")
+			g_DecompressFlags = squish::kDxt5;
+		else if(s == "-separate")
+			g_bSeparate = true;
+		else if(s == "-col-only")
+		{
+			g_bColOnly = true;
+			g_bMulOnly = false;
+			g_bSeparate = true;
+		}
+		else if(s == "-mul-only")
+		{
+			g_bMulOnly = true;
+			g_bColOnly = false;
+			g_bSeparate = true;
+		}
+		else if(s == "-nopiece")
+			g_bPieceTogether = false;
+		else
+			sFilenames.push_back(s);
 	}
+	//Decompress ANB files
+	for(list<string>::iterator i = sFilenames.begin(); i != sFilenames.end(); i++)
+		splitImages((*i).c_str());
 	FreeImage_DeInitialise();
 	return 0;
 }
